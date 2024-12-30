f41cfaf0b3eb5be91bd2e0e355a4f361db4eaac7
DNAMS1 <2110062@acuq.ae>
2024-12-31 02:55:08 +0400

    fixed

--- ThAmCo.Products/Program.cs
+++ ThAmCo.Products/Program.cs
@@ -1,22 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.IdentityModel.Tokens;
-using System.Security.Claims;
-
-var builder = WebApplication.CreateBuilder(args);
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-builder.Services
-    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.Authority = builder.Configuration["Auth:Authority"];
-        options.Audience = builder.Configuration["Auth:Audience"];
-    });
 builder.Services.AddAuthorization();
 
 var app = builder.Build();
